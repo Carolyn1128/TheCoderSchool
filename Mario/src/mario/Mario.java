74d55df799a23d827be5c7d5248c7b394158a0be
KevinYamamoto <31107034+KevinYamamoto@users.noreply.github.com>
2018-06-30 11:40:12 -0700

    replace

--- Mario/src/mario/Mario.java
+++ Mario/src/mario/Mario.java
@@ -5,17 +5,60 @@
  */
 package mario;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
 /**
  *
  * @author kevinyamamoto
  */
-public class Mario {
-
+public class Mario extends JApplet implements Runnable{
+    Thread thread;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    
+    @Override
+    public void init(){
+        setSize(600,600);
+        setBackground(Color.white);
+        setFocusable(true);
+        System.out.println("Hello Worldi");
+        
+    }
+
+    @Override
+    public void run() {
+        Thread game = Thread.currentThread();
+        System.out.println("Hello Worldr");
+        while(thread == game){
+            repaint();
+            try{
+                thread.sleep(25);
+            }   catch(InterruptedException e){
+                break;
+            }
+        }
     }
     
+    
+    @Override
+    public void paint(Graphics g){
+        g.setColor(Color.white);
+        System.out.println("Hello Worldp");
+        g.drawString("It's a me, Mario", 300,300);
+    }
+    
+    @Override
+    public void start(){
+        thread = new Thread(this);
+        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.start();
+       
+    }
+    
+    
 }
+
