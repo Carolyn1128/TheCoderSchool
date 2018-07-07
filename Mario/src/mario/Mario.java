f60df4dfedf5167394020273f04ffd146d38756d
KevinYamamoto <31107034+KevinYamamoto@users.noreply.github.com>
2018-07-07 11:01:51 -0700

    ItsAMeMario

--- Mario/src/mario/Mario.java
+++ Mario/src/mario/Mario.java
@@ -7,6 +7,12 @@ package mario;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 
 /**
@@ -19,14 +25,27 @@ public class Mario extends JApplet implements Runnable{
      * @param args the command line arguments
      */
 
-    
+    Graphics2D gfx;
+    BufferedImage bgd;
+    BufferedImage playerimg,imge;
+    Player player;
+            
+            
     @Override
     public void init(){
-        setSize(600,600);
+        try {
+           bgd = ImageIO.read(Mario.class.getResource("img/plain-blue-background.jpg"));
+           playerimg = ImageIO.read(Mario.class.getResource("img/Marior.png"));
+           player = new Player(playerimg, 450,250);
+           
+        } catch (IOException ex) {
+            Logger.getLogger(Mario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        setSize(900,500);
         setBackground(Color.white);
         setFocusable(true);
-        System.out.println("Hello Worldi");
-        
+        //System.out.println("Hello Worldi")
     }
 
     @Override
@@ -43,13 +62,22 @@ public class Mario extends JApplet implements Runnable{
         }
     }
     
-    
+   
     @Override
     public void paint(Graphics g){
+
+      
+        if(bgd != null){
+           g.drawImage(bgd, 0,0,900,500, this);
+        }
+        else{
+            System.out.println("bgd broke");
+        }
         g.setColor(Color.white);
-        System.out.println("Hello Worldp");
-        g.drawString("It's a me, Mario", 300,300);
+        //g.drawString("It's a me, Mario", 300,300);
+        player.draw(g,this);
     }
+ 
     
     @Override
     public void start(){
--- Mario/src/mario/Player.java
+++ Mario/src/mario/Player.java
@@ -5,10 +5,35 @@
  */
 package mario;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author kevinyamamoto
  */
-public class Player {
+public class Player extends ObjPos implements Observer {
+    BufferedImage img;
+    int x,y;
+    public Player(BufferedImage img, int x, int y) {
+        super(img, x, y);
+        this.img = img;
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public void draw (Graphics w, ImageObserver obs){
+        System.out.println(x+"and"+y);
+        w.drawImage(img, x, y, obs);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        
+    }
     
 }
